2b1dfb9e928cb0b260fc9cf2aab4ef764c12bace
marvincontreras0 <64647648+marvincontreras0@users.noreply.github.com>
2020-08-09 15:12:52 -0600

    Agregando  Codigo de spin de la ruleta

--- NivelUnoScene.cpp
+++ NivelUnoScene.cpp
@@ -1,9 +1,15 @@
 //Leo Pazzetti
 #include "NivelUnoScene.h"
 #include "MapScene.h"
+#include "extensions/cocos-ext.h"
+#include"ui/CocosGUI.h"
+#include <thread>
+#include <iostream>
 
 USING_NS_CC;
+USING_NS_CC_EXT;
 
+using namespace std;
 Scene* NivelUnoScene::createScene()
 {
     // 'scene' is an autorelease object
@@ -32,23 +38,24 @@ bool NivelUnoScene::init()
     Size visibleSize = Director::getInstance()->getVisibleSize();
     Point origin = Director::getInstance()->getVisibleOrigin();
 
-    auto menuItem1 = MenuItemFont::create("GoBack", CC_CALLBACK_1(NivelUnoScene::GoBack, this));
+    ui::Button *botonS = ui::Button::create("images/button1.png", "images/button2.png");
+    botonS->setPosition(Vec2(visibleSize.width / 2 + origin.x, (visibleSize.height / 5 + origin.y) - 25));
 
-    menuItem1->setPosition(Point(visibleSize.width / 2, (visibleSize.height / 2) * 1));
 
-    auto mapa = Menu::create(menuItem1, NULL);
-    mapa->setPosition(Point(0, 0));
-    this->addChild(mapa);
-    //David
-    auto label = Label::createWithTTF("PRIMER JUEGO", "fonts/Marker Felt.ttf", 24);
-    if (label != nullptr) {
-        // position the label on the center of the screen
-        label->setPosition(Vec2(origin.x + visibleSize.width / 2,
-            origin.y + visibleSize.height - label->getContentSize().height));
+   auto menuItem1 = MenuItemFont::create("GoBack", CC_CALLBACK_1(NivelUnoScene::GoBack, this));
 
-        // add the label as a child to this layer
-        this->addChild(label, 1);
-    }
+   menuItem1->setPosition(Point(visibleSize.width / 5, (visibleSize.height / 5) -25));
+
+   auto mapa = Menu::create(menuItem1, NULL);
+   mapa->setPosition(Point(0, 0));
+   Ruleta = Sprite::create("images/wheel.png");
+   Ruleta->setPosition(Point((visibleSize.width / 2), (visibleSize.height / 2) + 50));
+   this->addChild(Ruleta);
+   this->addChild(mapa);
+   this->addChild(botonS);
+   botonS->addTouchEventListener( CC_CALLBACK_2( NivelUnoScene::spinR, this ) );
+
+  
 
     return true;
 }
@@ -71,4 +78,39 @@ void NivelUnoScene::menuCloseCallback(Ref* pSender)
 #if (CC_TARGET_PLATFORM == CC_PLATFORM_IOS)
     exit(0);
 #endif
+}
+
+void NivelUnoScene::spinR(Ref* sender, ui::Widget::TouchEventType type) 
+{
+    auto rotacion = RotateBy::create(2, 1800);
+    auto rotacion2 = RotateBy::create(2, 50);
+    int r1 = 0;
+
+    switch (type)
+    {
+    case cocos2d::ui::Widget::TouchEventType::BEGAN:
+        log("boton pressed");
+        break;
+    case cocos2d::ui::Widget::TouchEventType::MOVED:
+        log("touch moved ");
+        break;
+    case cocos2d::ui::Widget::TouchEventType::ENDED:  
+        log("Touch ended");
+        r1 = (rand() % 2160) + 2000;
+        cout << r1 << endl;
+        rotacion = RotateBy::create(2, r1);
+        Ruleta->runAction(rotacion);
+        r1 = (rand() % 359) + 1;
+        cout << r1 << endl;
+        rotacion2 = RotateBy::create(1, r1);
+        log("Se roto");
+        log(r1);  
+        Ruleta->runAction(rotacion2);
+        break;
+    case cocos2d::ui::Widget::TouchEventType::CANCELED:
+        log("touch Canceled");
+        break;
+    default:
+        break;
+    }
 }
