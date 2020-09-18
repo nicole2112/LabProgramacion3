c7adb87e689d9d1f83aad190a0cbe1096c3bd1ef
nicole2112 <61556869+nicole2112@users.noreply.github.com>
2020-09-18 09:28:30 -0600

    actualización kant scene

--- KantScene.h
+++ KantScene.h
@@ -10,7 +10,7 @@ USING_NS_CC_EXT;
 
 class KantScene : public cocos2d::Layer {
 public:
-    // there's no 'id' in cpp, so we recommend returning the class instance pointer
+     // there's no 'id' in cpp, so we recommend returning the class instance pointer
     static cocos2d::Scene* createScene();
 
     // Here's a difference. Method 'init' in cocos2d-x returns bool, instead of returning 'id' in cocos2d-iphone
@@ -35,20 +35,35 @@ public:
     Sprite* bgItemC;
     Sprite* bgItemD;
 
+    //Para guardar la respuesta correcta
+    int respCorrecta;
+
+    bool respondido;
+
     //Variable que determina si se cambia de escuela
     bool cambiarEscena;
+    int contadorPreguntas;
+
+    void finEscena();
     
     // Funciones para inicializar teclado y manejar sus eventos
     void inicializarTeclado(void);
     void presionarTecla(EventKeyboard::KeyCode key, Event* event);
 
+    Sprite* feedback;
+    void respuestaCorrecta();
+    void respuestaIncorrecta();
+
     void GoBack(Ref* pSender);
+    void showDescartes(Ref* pSender);
+
     std::vector<std::vector<std::string>> vectorEpis;
     void cargarPreguntasEpis();
     void showQuestion(Ref*);
     bool checkrep(int, int[]);
     bool checkpreg(int, int[], int);
-    int A[8];
+
+    int A[6];
 };
 
 #endif // !KANT_SCENE_H
